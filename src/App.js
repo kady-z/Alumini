b3ad50896dd453c29e9fc9a122957a6e6382e6d3
unknown <rahulshiksingh@gmail.com>
2022-05-29 00:58:59 +0530

    Routes For Events Created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from './components/Home/Home';
-import Events from "./components/Events/Events";
+import UpcommingEvents from "./components/Events/UpcommingEvents";
+import PastEvents from "./components/Events/PastEvents";
 import Header from "./components/Header/Header";
 import Role from "./components/Roles/Roles"
 
@@ -16,7 +17,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="Events" element={<Events />} />
+          <Route path="UpcommingEvents" element={<UpcommingEvents />} />
+          <Route path="PastEvents" element={<PastEvents />} />
           <Route path="Role" element={<Role />} />
         </Routes>
       </BrowserRouter>
